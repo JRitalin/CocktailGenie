ea43d723f0fea3bdec831602d04472ab531765e4
JRitalin <r33drift@gmail.com>
2020-04-29 22:33:53 -0400

    login activity created, home activity created, firebase code implemented

---
+++ app/src/main/java/com/example/cocktailgenie/LoginScreen.java
@@ -0,0 +1,124 @@
+package com.example.cocktailgenie;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginScreen extends AppCompatActivity implements View.OnClickListener {
+
+
+    //buttons
+    public Button
+            emailSignInButton,
+            emailCreateAccountButton;
+
+
+    public EditText
+            fieldEmail,
+            fieldPassword;
+
+    // Firebase Auth
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+
+        fieldEmail = findViewById(R.id.fieldEmail);
+        fieldPassword=findViewById(R.id.fieldPassword);
+
+        // Buttons
+        emailSignInButton = findViewById(R.id.emailSignInButton);
+        emailCreateAccountButton = findViewById(R.id.emailCreateAccountButton);
+
+        emailSignInButton.setOnClickListener(this);
+        emailCreateAccountButton.setOnClickListener(this);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+            finish();
+            startActivity(new Intent(getApplicationContext(), Home.class));
+        }
+    }
+
+    private void registerUser() {
+        String email = fieldEmail.getText().toString().trim();
+        String password = fieldPassword.getText().toString().trim();
+
+        //checking if email and passwords are empty
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //create a new user
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(LoginScreen.this, "Registration Successful", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(LoginScreen.this, "Registration Error, Check Email and Password.", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+    }
+
+    //method for user login
+    private void userLogin() {
+        String email = fieldEmail.getText().toString().trim();
+        String password = fieldPassword.getText().toString().trim();
+
+        //checking if email and passwords are empty
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //logging in the user
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        finish();
+                        startActivity(new Intent(getApplicationContext(), Home.class));
+                    }
+
+                });
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == emailSignInButton) {
+            userLogin();
+        }
+
+        if (v == emailCreateAccountButton) {
+            registerUser();
+        }
+    }
+}
--- app/src/main/java/com/example/cocktailgenie/WelcomeSplash.java
+++ app/src/main/java/com/example/cocktailgenie/WelcomeSplash.java
@@ -6,17 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class WelcomeSplash extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 2500;//Milliseconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.welcome_splash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent (MainActivity.this,HomeActivity.class);
+                Intent homeIntent = new Intent (WelcomeSplash.this, LoginScreen.class);
                 startActivity(homeIntent);
                 overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
                 finish();
